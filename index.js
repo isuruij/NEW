3c5da032305b370129a97dbfcb7e584a632cce71
isuruij <2020uelog@gmail.com>
2024-11-06 02:46:56 +0530

    added email verification

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ AWS.config.update({ region: 'eu-north-1' });
 const cognito = new AWS.CognitoIdentityServiceProvider();
 const dynamoDB = new AWS.DynamoDB.DocumentClient();
 const CLIENT_ID = '5u8pmrkti2j5hsntkm5srtkb3v';
-const USER_POOL_ID = 'eu-north-1_SBN2he5bz'; // Replace with your user pool ID
-const TABLE_NAME = 'Users'; // Replace with your DynamoDB table name
+const USER_POOL_ID = 'eu-north-1_SBN2he5bz'; 
+const TABLE_NAME = 'Users'; 
 
 app.post('/signup', async (req, res) => {
   const { username, password, email, name, age } = req.body;
@@ -32,11 +32,7 @@ app.post('/signup', async (req, res) => {
 
     const data = await cognito.signUp(params).promise();
 
-    // Automatically confirm the user
-    await cognito.adminConfirmSignUp({
-      UserPoolId: USER_POOL_ID,
-      Username: username,
-    }).promise();
+
 
     // Store additional attributes in DynamoDB
     const dynamoParams = {
@@ -95,21 +91,21 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.post('/resend-confirmation', async (req, res) => {
-  const { username } = req.body;
+// app.post('/resend-confirmation', async (req, res) => {
+//   const { username } = req.body;
 
-  try {
-    const params = {
-      ClientId: CLIENT_ID,
-      Username: username,
-    };
+//   try {
+//     const params = {
+//       ClientId: CLIENT_ID,
+//       Username: username,
+//     };
 
-    const data = await cognito.resendConfirmationCode(params).promise();
-    res.json(data);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+//     const data = await cognito.resendConfirmationCode(params).promise();
+//     res.json(data);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
 
 app.get('/getuser/:username', async (req, res) => {
     const { username } = req.params;
